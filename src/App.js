f3df635621357f85ad1f05e874c3835dbb1e0028
Harsh Mehta <harshmehta813@gmail.com>
2021-12-24 17:05:50 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import useTimer from "./hook/useTimer";
+import "./styles.css";
+
+export default function App() {
+	const { value, startTimer, pauseTimer, resetTimer } = useTimer({
+		initialValue: 20
+	});
+	return (
+		<div className="App">
+			<div className="timer">
+				<h1>Timer</h1>
+				<h2>{value}!</h2>
+				<button onClick={startTimer}>START</button>
+				<button onClick={pauseTimer}>PAUSE</button>
+				<button onClick={resetTimer}>RESET</button>
+			</div>
+		</div>
+	);
+}
